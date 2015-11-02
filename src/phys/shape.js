eef1044b49711e648f7b31cb676adf2a538deed6
Eric Liang <etlgfx@gmail.com>
2015-11-02 08:30:43 -0500

    adding some skeleton codes

---
+++ src/phys/shape.js
@@ -0,0 +1,22 @@
+function PhysShape () {
+}
+
+PhysShape.prototype = {
+    density: 0,
+    radius: 0,
+
+    intersectPoint: function () {
+    },
+
+    /*
+     * wtf
+    raycast: function () {
+    }
+    */
+
+    aabb: function () {
+    },
+
+    mass: function () {
+    }
+};
